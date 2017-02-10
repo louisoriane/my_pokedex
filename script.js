a2789b07031cd3ff1a998a96c99d799ef4655bbc
Oriane Louis <oriane.louis@supinternet.fr>
2017-02-10 19:12:56 +0100

    Improve Ajax request + edit css

--- script.js
+++ script.js
@@ -9,65 +9,69 @@ function firstLetterToLowerCase(pokemon) {
 }
 
 function displayPokemonData(type, name, pokemonName, dataKey) {
-	$('#type').append(type);
-    $('#name').append(name);
+    document.getElementById('name').innerHTML = name;
+    document.getElementById('type').innerHTML = type;
     switch(dataKey) {
-    	case 29:
-    		pokemonName = "nidoran-f";
-    		break;
-    	case 32:
-    		pokemonName = "nidoran-m";
-    		break;
-    	case 83:
-    		pokemonName = "farfetchd";
-    		break;
-    	case 122:
-    		pokemonName = "mr-mime";
-    		break;
+     case 29:
+         pokemonName = "nidoran-f";
+         break;
+     case 32:
+         pokemonName = "nidoran-m";
+         break;
+     case 83:
+         pokemonName = "farfetchd";
+         break;
+     case 122:
+         pokemonName = "mr-mime";
+         break;
     }
-    $('.screen').append("<img src='http://img.pokemondb.net/artwork/"+ pokemonName +".jpg' class='image'/>");
+    document.getElementById('screen').innerHTML = "<img src='http://img.pokemondb.net/artwork/"+ pokemonName +".jpg' class='image'/>";
 }
 
-$(function() {
-	$('form').submit(function() {
-		var i = 1;
-		var pokemonStatement = false;
-		var pokemon = $('input[type=text]').val();
-		pokemon = firstLetterToUpperCase(pokemon);
-		$.ajax({
-			url : 'pokemons.json',
-			type : 'GET',
-			dataType : 'json',
-			success : function(response) {
-				// Clear all div to not keep previous display
-				$('#type').empty();
-				$('#name').empty();
-				$('#error').empty();
-				$('.screen').empty();
+function displayError(pokemon, pokemonStatement) {
+    document.getElementById('error').innerHTML = "";
+    if (!pokemonStatement && Number.isInteger(pokemon)) {
+        document.getElementById('error').innerHTML = "<span id='message'>Pokemon number " + pokemon + " not found</span>";
+    } else if (!pokemonStatement) {
+        document.getElementById('error').innerHTML = "<span id='message'>" + pokemon + " not found</span>";
+    }
+}
 
-				// Browse response (pokemon object) to find if the user's pokemon exist
-				for (var dataKey in response){
-					var pokemonName = firstLetterToLowerCase(response[dataKey].name);
+window.onload = function() {
+    document.forms['form'].onsubmit = function() {
+        var i = 1;
+        var pokemon = document.getElementById('pokemon-number-name').value;
+        pokemon = firstLetterToUpperCase(pokemon);
+        var pokemonStatement = false;
+        var http = new XMLHttpRequest();
+        var url = "pokemons.json";
+        http.open("GET", url, true);
+        http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        // Request AJAX -> Browse the file to find user's pokemon exist
+        http.onload = function() {
+            document.getElementById('name').innerHTML = "";
+            document.getElementById('type').innerHTML = "";
+            document.getElementById('screen').innerHTML = "";
+            if(http.readyState == 4 && http.status == 200) {
+                var response = JSON.parse(http.responseText);
+                for (var dataKey in response){
+                    var pokemonName = firstLetterToLowerCase(response[dataKey].name);
                     if (response[dataKey].name === pokemon) {
-                    	displayPokemonData(response[dataKey].type, response[dataKey].name, pokemonName, i);
-                    	pokemonStatement = true;
-                    	break;
+                        displayPokemonData(response[dataKey].type, response[dataKey].name, pokemonName, i);
+                        pokemonStatement = true;
+                        break;
                     } else if (dataKey === pokemon) {
-                    	displayPokemonData(response[dataKey].type, response[dataKey].name, pokemonName, i);
-                    	pokemonStatement = true;
-                    	break;
+                        displayPokemonData(response[dataKey].type, response[dataKey].name, pokemonName, i);
+                        pokemonStatement = true;
+                        break;
                     }
                     i++;
-               	}
-
-               	// If user's pokemon not found, display msg error
-                if (!pokemonStatement && $.isNumeric(pokemon)) {
-                	$('#error').append("<span id='message'>Pokemon number " + pokemon + " not found</span>");
-                } else if (!pokemonStatement) {
-                	$('#error').append("<span id='message'>" + pokemon + " not found</span>");
                 }
-			}
-		});
-		return false;
-	});
-});
+                displayError(pokemon, pokemonStatement);
+            }
+        }
+        http.send();
+        return false;
+    };
+};
